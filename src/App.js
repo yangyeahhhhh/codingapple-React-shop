046805e40a0f48aaea6497c011ef3c973e25b354
yangyeahhhhh <112038515+yangyeahhhhh@users.noreply.github.com>
2023-01-01 01:20:11 +0900

    Part 2 4강 - 7강

--- src/App.js
+++ src/App.js
@@ -1,48 +1,92 @@
+/*eslint-disable*/
 import { useState } from 'react';
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Detail from './routes/Detail.js'
 
 function App() {
 
   let [shoes] = useState(data)
+  let navigate = useNavigate();
 
   return (
     <div className="App">
+
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/detail') }}>Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+              {/* <Card shoes={shoes[0]} i={1}></Card>
+              <Card shoes={shoes[1]} i={2}></Card>
+              <Card shoes={shoes[2]} i={3}></Card> */}
+                {shoes.map((a, i)=>{
+                  return (
+                    <Card shoes={shoes[i]} i={i+1} key={i}></Card>
+                  )})
+                }
+                </div>
+              </div>
+          </>
+        }></Route>
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>}></Route>
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>}></Route>
+          <Route path="location" element={<div>위치정보임</div>}></Route>
+        </Route>
+        <Route path="/event" element={<EventPage />}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+        </Route>
+        <Route path="*" element={<div>없는페이지요</div>}></Route>
+      </Routes>
 
     </div>
   );
+}
+
+function EventPage(){
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function About(){
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes'+props.i+'.jpg'} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,28 @@
+/*eslint-disable*/
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+
+  let {id} = useParams();
+  let 찾은상품 = props.shoes.find(function(x){
+    return x.id == id
+  })
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+    </div> 
+  )
+}
+
+export default Detail;
